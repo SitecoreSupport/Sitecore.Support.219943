bc859569980451f315b22dfc7d8e9b8150e28dfc
ArtemNizelnyk <arn@sitecore.net>
2018-05-05 17:01:53 +0300

    Init Project with original code

---
+++ src/Sitecore.Support.219943/Modules/EmailCampaign/Core/Pipelines/GenerateLink/Hyperlink/FixedGenerateHyperlink.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitecore.Support.Modules.EmailCampaign.Core.Pipelines.GenerateLink.Hyperlink
+{
+    using Diagnostics;
+    using Sitecore.Modules.EmailCampaign.Core.Pipelines.GenerateLink;
+    using Sitecore.Modules.EmailCampaign.Core.Pipelines.GenerateLink.Hyperlink;
+
+    public class FixedGenerateHyperlink : GenerateHyperlink
+    {
+        public override void Process(GenerateLinkPipelineArgs args)
+        {
+            Assert.IsNotNull(args, "Arguments can't be null");
+            Assert.IsNotNull(args.Url, "Url can't be null");
+            base.Process(args);
+        }
+    }
+}
