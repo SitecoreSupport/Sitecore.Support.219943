a97b68e3d9eac71c0aaa634ef6d4c891f9d03bc9
DK\dmsa <dmsa@sitecore.net>
2018-09-04 12:19:24 +0300

    Update the patch for issue #219943

---
+++ src/Sitecore.Support.219943/Modules/EmailCampaign/Core/Pipelines/GenerateLink/Hyperlink/EncryptQueryString.cs
@@ -0,0 +1,27 @@
+﻿namespace Sitecore.Support.Modules.EmailCampaign.Core.Pipelines.GenerateLink.Hyperlink
+{
+	using Sitecore.Modules.EmailCampaign.Core.Crypto;
+	using Sitecore.Modules.EmailCampaign.Core.Pipelines.GenerateLink;
+	using System.Net;
+	using OriginEncryptQueryString = Sitecore.Modules.EmailCampaign.Core.Pipelines.GenerateLink.Hyperlink.EncryptQueryString;
+
+	public class EncryptQueryString : OriginEncryptQueryString
+	{
+		public EncryptQueryString(QueryStringEncryption queryStringEncryption)
+			: base(queryStringEncryption)
+		{
+		}
+
+		public override void Process(GenerateLinkPipelineArgs args)
+		{
+			if (args.PreviewMode)
+			{
+				args.GeneratedUrl = WebUtility.HtmlDecode(args.Url);
+			}
+			else
+			{
+				base.Process(args);
+			}
+		}
+	}
+}
